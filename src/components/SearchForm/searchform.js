b2164699e1eecd574e149da4eef8d3eb0fb79c4d
nicoleremy95 <nicoleremy95@gmail.com>
2020-08-12 13:40:42 -0700

    changed file names, added resultdirectory, searchresultcontainer,and searchform components. added a utils folder with an api.js

---
+++ src/components/SearchForm/searchform.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SearchForm(props){
+    return(
+
+    )
+}
+
+export default SearchForm
--- src/components/SearchResultContainer/searchresultcontainer.js
+++ src/components/SearchResultContainer/searchresultcontainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchForm from "./SearchForm/searchform"
 
 
 export default function directory() {
