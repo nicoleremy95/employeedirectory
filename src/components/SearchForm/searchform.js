2ee853482bc400afba56cbb8088e85dd8be871a6
nicoleremy95 <nicoleremy95@gmail.com>
2020-08-12 13:51:22 -0700

    added search form

--- src/components/SearchForm/searchform.js
+++ src/components/SearchForm/searchform.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function SearchForm(props){
     return(
-
+        <form>
+            <div>
+                <label htmlFor="search">Search:</label>
+                <input
+                    onChange={props.handleInputChange}
+                    value={props.search}
+                    name="search"
+                    type="text"
+                    placeholder="search by name"
+                    id="search"
+                />
+                <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+                    Search
+                </button>
+            </div>
+        </form>
     )
 }
 
